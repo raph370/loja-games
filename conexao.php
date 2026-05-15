e50e5b06ccab1e550059969ae20d566bdf214d57
raphael frança <raphaelrsfranca@gmail.com>
2026-05-15 22:07:58 +0000

    conecta banco Railway

--- conexao.php
+++ conexao.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'localhost';
-$usuario = 'admin';
-$senha = 'senha123';
-$banco = 'loja_games';
+$host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
+$usuario = getenv('MYSQLUSER') ?: 'root';
+$senha = getenv('MYSQLPASSWORD') ?: 'snLhcUzQdrSgdIYmmwMValauxOgDEtWN';
+$banco = getenv('MYSQLDATABASE') ?: 'railway';
+$porta = getenv('MYSQLPORT') ?: '3306';
 
-$conn = mysqli_connect($host, $usuario, $senha, $banco);
+$conn = mysqli_connect($host, $usuario, $senha, $banco, $porta);
 
 if (!$conn) {
     die('Erro na conexão: ' . mysqli_connect_error());
